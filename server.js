5806e7cad1af9b5a7837fcba8ada11fe3dcf51bf
timestep <jbosshellokitty@gmail.com>
2013-06-23 16:37:49 -0400

    heroku fixes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var util = require('util'),
     connect = require('connect'),
-    port = 1337;
+    port = 8080;
 
 connect.createServer(connect.static(__dirname)).listen(port);
 util.puts('Listening on ' + port + '...');
