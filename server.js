208c83753fc7816316ae68610189d8057863227e
timestep <jbosshellokitty@gmail.com>
2013-06-23 17:04:01 -0400

    express update

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-var util = require('util'),
-    connect = require('connect'),
-    port = 5000;
+// var util = require('util'),
+//     connect = require('connect'),
+//     port = 5000;
 
-connect.createServer(connect.static(__dirname)).listen(port);
-util.puts('Listening on ' + port + '...');
-util.puts('Press Ctrl + C to stop.');
+// connect.createServer(connect.static(__dirname)).listen(port);
+// util.puts('Listening on ' + port + '...');
+// util.puts('Press Ctrl + C to stop.');
+
+var express = require("express");
+var server = express.createServer();
+server.configure(function(){
+  server.use(express.static(__dirname));
+});
+
+server.listen(5000);
+
+console.log("Listening on Port 5000")
